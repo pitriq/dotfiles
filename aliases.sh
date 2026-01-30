c840375ad74e1c9f23163939c7facc09597b0a05
Francisco Pitriqueo <franpitri@gmail.com>
2026-01-30 17:50:21 -0300

    🧑‍💻

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,7 @@ alias flac2mp3='flac2mp3_func() { ffmpeg -i "$1" -b:a 320k "${1%.*}.mp3"; }; fla
 alias dev="cd $HOME/Developer"
 alias dots="cd $HOME/Developer/me/dotfiles"
 alias work="cd $HOME/Developer/apps/sytex"
+alias sb="cd $HOME/Developer/sandbox"
 alias dl="cd $HOME/Downloads"
 alias brewdump="brew bundle dump --formula --cask --tap"
 alias baptise="sudo xattr -d com.apple.quarantine"
