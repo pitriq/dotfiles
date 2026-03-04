76e273a67f9e7de5e1cb43ace7d1d7a544991aa2
Francisco Pitriqueo <franpitri@gmail.com>
2026-03-04 01:03:19 -0300

    🧑‍💻

--- aliases.sh
+++ aliases.sh
@@ -6,6 +6,7 @@
 
 alias ytdl="yt-dlp -x --audio-format mp3"
 alias flac2mp3='flac2mp3_func() { ffmpeg -i "$1" -b:a 320k "${1%.*}.mp3"; }; flac2mp3_func'
+alias mov2mp4='mov2mp4_func() { ffmpeg -i "$1" -c:v libx264 -c:a aac "${1%.*}.mp4"; }; mov2mp4_func'
 alias dev="cd $HOME/Developer"
 alias dots="cd $HOME/Developer/me/dotfiles"
 alias work="cd $HOME/Developer/apps/sytex"
